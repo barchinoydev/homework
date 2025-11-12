2e68ac59c7eda4a15988a3ebaf0171a4178abcf9
barchinoydev <barchinoyjumyozova@gmail.com>
2025-11-13 00:42:38 +0500

    main.py
    fayl qo'shdim

---
+++ son.py
@@ -0,0 +1,23 @@
+#################1################
+# son = int(input("Istalgan son kiriting: "))
+# kvadrat = son**2
+# kub = son**3
+# print(f"{son} ning kvadrati {kvadrat} ga teng")
+# print(f"{son} ning kubi {kub} ga teng")
+
+###################2#####################
+# son = int(input("Yoshingiz nechida? "))
+# yili = 2025 - son
+# print(f"Siz {yili} - yilda tug'ilgansz")
+
+##############3##############
+# bson = int(input("Birinchi sonni kiriting>>> "))
+# ison = int(input("Ikkinchi sonni kiriting>>> "))
+# a = bson  + ison
+# b = bson - ison
+# c = bson * ison
+# d = bson / ison
+# print(a)
+# print(b)
+# print(c)
+# print(d)
