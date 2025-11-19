3a4397cab89e450527bd17cf61472384033669c2
barchinoydev <barchinoyjumyozova@gmail.com>
2025-11-19 22:57:21 +0500

    main py
    fayl qo'shdim

---
+++ Python_mod.py
@@ -0,0 +1,110 @@
+# 1. ceil(x)
+import math
+
+# x = 3.2
+# print(math.ceil(x))
+#
+# x = -2.7
+# print(math.ceil(x))
+
+
+
+   # 2. fabs(x)
+# import math
+#
+# x = -5.9
+# print(math.fabs(x))
+#
+# x = 5
+# print(math.fabs(x))
+
+
+
+    # 3. floor(x)
+# import math
+#
+# x = 4.9
+# print(math.floor(x))
+#
+# x = 3.1
+# print(math.floor(x))
+
+
+
+  # 4. exp(x)
+# import math
+#
+# x = 7
+# print(math.exp(x))
+#
+# x = 5
+# print(math.exp(x))
+
+
+
+#   # 5. cos(x)
+# import math
+#
+# x = 0
+# print(math.cos(x))
+# x=math.pi
+# print(math.cos(x))
+#
+
+
+#    # 6. sin(x)
+# import math
+#
+# x = 0
+# print(math.sin(x))
+#
+# x = math.pi / 2
+# print(math.sin(x))
+#
+
+
+#   # 7. tan(x)
+# import math
+#
+# x = 0
+# print(math.tan(x))
+#
+# x = math.pi / 4
+# print(math.tan(x))
+#
+
+
+
+#    # 8. degrees(x)
+# import math
+#
+# x = math.pi
+# print(math.degrees(x))
+#
+# x = math.pi / 6
+# print(math.degrees(x))
+#
+
+
+
+
+  # 9. radians(x)
+# import math
+#
+# x = 180
+# print(math.radians(x))
+#
+# x = 45
+# print(math.radians(x))
+
+
+
+
+  # 10. e (konstanta)
+import math
+
+# x = math.e
+# print(x)
+#
+# x = math.e ** 2
+# print(x)
